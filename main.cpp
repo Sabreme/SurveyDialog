5d905ee34d4b5be37774a2846dbec33b6097d7de
Patrick <the.token.blackman@gmail.com>
2016-01-08 00:52:30 +0200

    FIRST COMMIT
    ADDED FILES

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "surveydialog.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    SurveyDialog w;
+    w.show();
+
+    return a.exec();
+}
