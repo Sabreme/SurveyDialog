4c46d615755c2288c57754c919a3ad752026cc3c
Sabreme <the.token.blackman@gmail.com>
2016-01-08 14:51:59 +0200

    DONE: Added GroupBox for Frame 1
    DONE: Got Selection Signalling sent from Question 1
    
    TODO: Get String Extractor from Name to Values
    
    TODO: Do Selection Signalling for all Questions
    
    TODO: DO UserTesting Integration

--- surveydialog.cpp
+++ surveydialog.cpp
@@ -1,14 +1,61 @@
 #include "surveydialog.h"
 #include "ui_surveydialog.h"
+#include <iostream>
 
 SurveyDialog::SurveyDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SurveyDialog)
 {
     ui->setupUi(this);
+    mapSignals();
 }
 
 SurveyDialog::~SurveyDialog()
 {
     delete ui;
+}
+
+void SurveyDialog::on_buttonSubmit_clicked()
+{
+
+    //std::cout << "Button Pressed" << std::endl;
+}
+
+//void SurveyDialog::selectionMade(QString boxName)
+//{
+//    std::cout << boxName.toStdString() << std::endl;
+//}
+
+void SurveyDialog::selectionMade(QAbstractButton *radioButton)
+{
+    std::cout << radioButton->objectName().toStdString() << std::endl;
+}
+
+
+void SurveyDialog::mapSignals()
+{
+//    mapper = new QSignalMapper(this);
+
+//    QObject::connect (this->ui->quest1Ans1, SIGNAL(clicked()),mapper,SLOT(map()));
+//    mapper->setMapping(this->ui->quest1Ans1, this->ui->quest1Ans1->objectName());
+
+//    connect(mapper,SIGNAL(mapped(QString)),this,SLOT(selectionMade(QString)));
+
+    group1 = new QButtonGroup(ui->frame_Choice1);
+    group1->addButton(ui->quest1Ans1);
+    group1->addButton(ui->quest1Ans2);
+    group1->addButton(ui->quest1Ans3);
+    group1->addButton(ui->quest1Ans4);
+    group1->addButton(ui->quest1Ans5);
+
+    QObject::connect(group1,SIGNAL(buttonClicked(QAbstractButton*)),this,SLOT(selectionMade(QAbstractButton*)));
+
+}
+
+
+
+void SurveyDialog::on_quest1Ans1_clicked()
+{
+
+
 }
--- surveydialog.h
+++ surveydialog.h
@@ -2,6 +2,11 @@
 #define SURVEYDIALOG_H
 
 #include <QDialog>
+#include <QFrame>
+#include <QCheckBox>
+#include <QSignalMapper>
+#include <QButtonGroup>
+
 
 namespace Ui {
 class SurveyDialog;
@@ -15,8 +20,25 @@ public:
     explicit SurveyDialog(QWidget *parent = 0);
     ~SurveyDialog();
 
+
+private slots:
+    void on_buttonSubmit_clicked();
+
+    //void selectionMade(QString boxName);
+    void selectionMade (QAbstractButton *radioButton);
+
+    void mapSignals();
+
+
+
+
+    void on_quest1Ans1_clicked();
+
 private:
     Ui::SurveyDialog *ui;
+
+    QSignalMapper * mapper;
+    QButtonGroup * group1;
 };
 
 #endif // SURVEYDIALOG_H
